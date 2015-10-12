6a2fe916d0554401434ca7151781c93318e547ed
zhuming3834 <zhuming3834@sina.com>
2015-10-12 19:16:04 +0800

    Initial Commit

---
+++ GaodeWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GaodeWeather
+//
+//  Created by HGDQ on 15/10/12.
+//  Copyright (c) 2015年 HGDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GaodeWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GaodeWeather
+//
+//  Created by HGDQ on 15/10/12.
+//  Copyright (c) 2015年 HGDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
